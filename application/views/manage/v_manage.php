741e2394596430e7ed8050b6c8ba2518702c0c81
Jay <sunhhjay@qq.com>
2016-03-31 20:46:15 +0800

    四月再说

--- application/views/manage/v_manage.php
+++ application/views/manage/v_manage.php
@@ -1,4 +1,5 @@
 <!-- 个人主页 -->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>static/css/font-awesome/font-awesome.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>static/css/manage.css" />
 <div class="manage-head-bg">
 	<div class="manage-head">
@@ -7,7 +8,7 @@
 			<!-- 这里是搜索框 -->
 			<form  method="post" name="keyWordSearch">
 				<input type="text" name="keySearch" id="keySearch" class="key-input" placeholder="搜索你感兴趣的新闻" required />
-				<button class="btn-submit" id="loginBtn">搜索</button>
+				<button class="btn-submit" id="loginBtn"><i class="fa fa-search"></i></button>
 			</form>
 		</div>
 		<!-- 标签 -->
@@ -46,26 +47,31 @@
 	<div class="manage-contain-left pull-left">
 		<!-- 个人信息的展示 border包含的地方 -->
 		<div class="contain-self-message">
-			<h3>黄小杰<span>这是你的个人一句话介绍</span></h3>
+			<h2>黄小杰<span> , 这是你的个人一句话介绍</span></h2>
 			<!-- 个人头像 -->
-			<div class="contain-self-headImg">
-				<img src="" alt="新闻" />
+			<div class="contain-self-headImg pull-left">
+				<img src="<?php echo base_url(); ?>static/upload/head.jpg" alt="新闻" />
 			</div>
-			<div class="contain-self-main">
-				<div class="contain-main-item">
-					<span>地址</span>
-					<span>行业</span>
-					<span>性别</span>
+			<div class="contain-self-main pull-left">
+				<div class="contain-main-item item-first">
+					<span class="boder-right"><i class="fa fa-map-marker"></i>云南省</span>
+					<span class="boder-right">计算机软件行业</span>
+					<span><i class="fa fa-mars"><!-- 男 --></i><!-- <i class="fa fa-venus">女</i> --></span>
 				</div>
 				<div class="contain-main-item">
-					<span>公司</span>
-					<span>职业</span>
+					<span class="boder-right"><i class="fa fa-building"></i>百度</span>
+					<span>java高级工程师</span>
 				</div>
 				<div class="contain-main-item">
-					<span>毕业学校</span>
-					<span>所学专业</span>
+					<span class="boder-right"><i class="fa fa-mortar-board"></i>中南民族大学</span>
+					<span>文学与新闻传播</span>
+				</div>
+				<div class="contain-main-item">
+					<span><i class="fa fa-hand-o-right"></i>你还想说点什么</span>
 				</div>
 			</div>
+
+			<div class="clear-fix"><!-- 清除浮动 --></div>
 			<!-- 点击可以查看下面的具体内容 -->
 			<div class="contain-nav">
 				<ul>
